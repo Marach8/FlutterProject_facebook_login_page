410bf5b05893d80366911b37abc566e1f12c07b4
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-08-22 05:39:16 +0100

    Restructured the code

--- login_page2/lib/login_page.dart
+++ login_page2/lib/login_page.dart
@@ -14,112 +14,111 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context){
     var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
 
     return Scaffold(      
-      body: Center(
+      body: SingleChildScrollView(
         child: Container(
           color: const Color.fromARGB(255, 5, 28, 50),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
+          height: h, width: w,
           child: Stack(
             children: [
               Positioned(
                 child: Center(
                   child: SingleChildScrollView(
                     child: SizedBox(                      
-                      height: MediaQuery.of(context).size.height*0.95,
-                      width: MediaQuery.of(context).size.width*0.95,
+                      height: h*0.95, width: w*0.95,
                       child: Column(
                         children: [
-                          SizedBox(
-                            height:MediaQuery.of(context).size.height*0.04
-                          ),
+                          SizedBox(height:h*0.04),
                           const Row(
                             children: [
-                              Icon(
-                                Icons.arrow_back_outlined,
-                                color: Colors.white,
-                              )
+                              Icon(Icons.arrow_back_outlined, color: Colors.white,)
                             ]
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.045
-                          ),
+                          SizedBox(height: h*0.045),
                           Text('English (US)',
                             style: GoogleFonts.getFont(
-                              'Ubuntu',
-                            color: Colors.white60,
-                            fontWeight: FontWeight.w100
+                              'Ubuntu', color: Colors.white60, fontWeight: FontWeight.w100
                             )
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.051
-                          ),
+                          SizedBox(height: h*0.051),
                           CircleAvatar(
-                            radius:MediaQuery.of(context).size.height*0.038,
+                            radius:h*0.038,
                             backgroundColor: const Color.fromARGB(255, 46, 108, 224),                    
                             child: Text(
                               'f',
                               style: TextStyle(
-                                fontSize: MediaQuery.of(context).size.height*0.08,
+                                fontSize: h*0.08,
                                 fontWeight: FontWeight.bold,
                                 color: const Color.fromRGBO(255, 255, 255, 0.8)
                               )
                             )
                           ), 
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.075
-                          ),
+                          SizedBox(height: h*0.075),
                           Container(
-                            padding: const EdgeInsets.only(
-                              left:9, right:9,
-                            ),
+                            padding: const EdgeInsets.only(left:9, right:9),
                             child: Column(
                               children: [
-                                const Divider(
-                                  height:2,
-                                  color: Colors.white60                    
-                                ),
+                                const Divider(height:2, color: Colors.white60),
+                                SizedBox(height: h*0.005),
                                 SizedBox(
-                                  height: MediaQuery.of(context).size.height*0.005
-                                ),
-                                SizedBox(                            
-                                  height: MediaQuery.of(context).size.height*0.073,
-                                  width: MediaQuery.of(context).size.height*0.9,                            
-                                  child: Text("""
-By proceeding, you agree to MTN's Terms, which
-includes letting Facebook request and receive your
-phone number. Change Settings""",
-                                  style: GoogleFonts.getFont(
-                                    'Nunito',
-                                    color: Colors.white,
-                                    fontSize: MediaQuery.of(context).size.height*0.0185,
-                                    fontWeight: FontWeight.w400
-                                    ),
+                                  height: h*0.073, width: w*0.9, 
+                                  child: Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'By proceeding, you agree to',
+                                          style: GoogleFonts.getFont(
+                                            'Nunito', color: Colors.white,
+                                            fontSize: h*0.0185,
+                                            fontWeight: FontWeight.w400
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: " MTN's Terms",
+                                          style: GoogleFonts.getFont(
+                                            'Nunito',
+                                            color: Colors.blue.shade400,
+                                            fontSize: h*0.0185,
+                                            fontWeight: FontWeight.w400
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: ', which includes letting Facebook request and receive your phone number.',
+                                          style: GoogleFonts.getFont(
+                                            'Nunito',
+                                            color: Colors.white,
+                                            fontSize: h*0.0185,
+                                            fontWeight: FontWeight.w400
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: ' Change Settings',
+                                          style: GoogleFonts.getFont(
+                                            'Nunito',
+                                            color: Colors.blue.shade500,
+                                            fontSize: h*0.0185,
+                                            fontWeight: FontWeight.w400
+                                          ),
+                                        )
+                                      ]
+                                    )
                                   ),
-                                ),                          
-                                Divider(
-                                  thickness: MediaQuery.of(context).size.height*0.0015,                      
-                                  color: Colors.white                   
                                 ),
+                                Divider(thickness: h*0.0015, color: Colors.white),
                               ]
                             )
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.004
-                          ),
+                          SizedBox(height: h*0.004),
                           Container(    
                             padding: const EdgeInsets.only(left:10),
-                            height: MediaQuery.of(context).size.height*0.072,
-                            width: MediaQuery.of(context).size.width*0.9,
+                            height: h*0.072, width: w*0.9,
                             decoration: BoxDecoration(
                               color: const Color.fromARGB(255, 22, 37, 52),
                               borderRadius: BorderRadius.circular(10),
                               boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.white,
-                                  blurRadius: 1
-                                )
+                                BoxShadow(color: Colors.white, blurRadius: 1)
                               ]
                             ),
                             child: TextField(
@@ -136,21 +135,16 @@ phone number. Change Settings""",
                               )
                             ),
                           ), 
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.02
-                          ),
+                          SizedBox(height: h*0.02),
                           Container(    
                             padding: const EdgeInsets.only(left:10),
-                            height: MediaQuery.of(context).size.height*0.072,
-                            width: MediaQuery.of(context).size.width*0.9,
+                            height: h*0.072,
+                            width: w*0.9,
                             decoration: BoxDecoration(
                               color: const Color.fromARGB(255, 22, 37, 52),
                               borderRadius: BorderRadius.circular(10),
                               boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.white,
-                                  blurRadius: 1
-                                )
+                                BoxShadow(color: Colors.white, blurRadius: 1)
                               ]
                             ),
                             child: TextField(
@@ -168,12 +162,9 @@ phone number. Change Settings""",
                               )
                             ),
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.02
-                          ),
+                          SizedBox(height: h*0.02),
                           SizedBox(                            
-                            height: MediaQuery.of(context).size.height*0.06,
-                            width: MediaQuery.of(context).size.width*0.9,
+                            height: h*0.06, width: w*0.9,
                             child: ElevatedButton(    
                               style: ButtonStyle(
                                 backgroundColor: const MaterialStatePropertyAll(Color.fromARGB(255, 46, 108, 224),),
@@ -182,51 +173,39 @@ phone number. Change Settings""",
                                     borderRadius: BorderRadius.circular(25)
                                   )
                                 )
-                              ),                              
-                              onPressed: (){print(MediaQuery.of(context).size.height);},
+                              ),
+                              onPressed: (){},
                               child: Text(
                                 'Log in',
                                 style: GoogleFonts.getFont(
-                                  'Quicksand',
-                                  color: Colors.white,
-                                  fontSize: h*0.02,
-                                  fontWeight: FontWeight.bold,
+                                  'Quicksand', color: Colors.white, fontSize: h*0.02, fontWeight: FontWeight.bold
                                 )
                               )
                             ),
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.005
-                          ),
+                          SizedBox(height: h*0.005),
                           SizedBox(
                             height: h*0.05,
                             child: TextButton(                              
                               child: Text(
                                 'Forgot password?',
                                 style: GoogleFonts.getFont(
-                                'Quicksand',
-                                color: Colors.white,
-                                fontSize: h*0.02,
-                                fontWeight: FontWeight.bold
-                              )
-                                ),
+                                  'Quicksand', color: Colors.white,
+                                  fontSize: h*0.02, fontWeight: FontWeight.bold
+                                )
+                              ),
                               onPressed: (){}                            
                             ),
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.09
-                          ),
+                          SizedBox(height: h*0.09),
                           Container(                             
                             decoration: const BoxDecoration(
                               color: Color.fromARGB(255, 5, 28, 50),
                               boxShadow: [
-                                BoxShadow(
-                                  blurRadius: 1,                                  
-                                )
+                                BoxShadow(blurRadius: 1)
                               ]
                             ),
-                            height: MediaQuery.of(context).size.height*0.06,
-                            width: MediaQuery.of(context).size.width*0.9,
+                            height: h*0.06,  width: w*0.9,
                             child: ElevatedButton(    
                               style: ButtonStyle(
                                 backgroundColor: const MaterialStatePropertyAll(
@@ -254,24 +233,19 @@ phone number. Change Settings""",
                               )
                             ),
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height*0.015
-                          ),
+                          SizedBox(height: h*0.03),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [                    
-                              FaIcon(FontAwesomeIcons.meta,
-                              color: Colors.white,
-                              size: h*0.02,
-                              ),
-                              SizedBox(
-                            width: MediaQuery.of(context).size.width*0.01
-                          ),
-                              Text('Meta',
-                              style: GoogleFonts.getFont(
-                                'Quicksand',
+                              FaIcon(
+                                FontAwesomeIcons.meta,
                                 color: Colors.white,
-                                fontSize: h*0.022
+                                size: h*0.02,
+                              ),
+                              SizedBox(width: w*0.01),
+                              Text('Meta',
+                                style: GoogleFonts.getFont(
+                                  'Quicksand', color: Colors.white, fontSize: h*0.022
                                 )
                               )
                             ]
@@ -284,7 +258,7 @@ phone number. Change Settings""",
               ),
             ]
           ),
-        )
+        ),
       )
     );
   }
